437ab7c989ab9e20765d792a78eb3ff2e01ea0e9
grechsteiner <grayson@fullscale.org>
2024-11-02 18:19:07 -0400

    Move factory

---
+++ Moves/BoardMoveFactory.cc
@@ -0,0 +1,23 @@
+// BoardMoveFactory.cc 
+
+#include "BoardMoveFactory.h"
+#include "BoardMove.h"
+#include "StandardBoardMove.h"
+#include "PromotionBoardMove.h"
+#include "CastleBoardMove.h"
+
+
+// Static
+std::unique_ptr<BoardMove> BoardMoveFactory::createStandardMove(BoardSquare const &fromSquare, BoardSquare const &toSquare, BoardSquare const &captureSquare, bool doesEnableEnpassant, PieceData const &movedPieceData, std::optional<PieceData> const &capturedPieceData) {
+    return std::make_unique<StandardBoardMove>(fromSquare, toSquare, captureSquare, doesEnableEnpassant, movedPieceData, capturedPieceData);
+}
+    
+// Static
+std::unique_ptr<BoardMove> BoardMoveFactory::createPromotionMove(BoardSquare const &fromSquare, BoardSquare const &toSquare, BoardSquare const &captureSquare, PieceType promotionPieceType, bool doesEnableEnpassant, PieceData const &movedPieceData, std::optional<PieceData> const &capturedPieceData) {
+    return std::make_unique<PromotionBoardMove>(fromSquare, toSquare, captureSquare, promotionPieceType, doesEnableEnpassant, movedPieceData, capturedPieceData);
+}
+    
+// Static
+std::unique_ptr<BoardMove> BoardMoveFactory::createCastleMove(BoardSquare const &fromSquare, BoardSquare const &toSquare, BoardSquare const &captureSquare, BoardSquare const &rookFromSquare, BoardSquare const &rookToSquare,  bool doesEnableEnpassant, PieceData const &movedPieceData, std::optional<PieceData> const &capturedPieceData) {
+    return std::make_unique<CastleBoardMove>(fromSquare, toSquare, captureSquare, rookFromSquare, rookToSquare, doesEnableEnpassant, movedPieceData, capturedPieceData);
+}
---
+++ Moves/BoardMoveFactory.h
@@ -0,0 +1,24 @@
+// BoardMoveFactory.h
+
+#ifndef BoardMoveFactory_h
+#define BoardMoveFactory_h
+
+#include <memory>
+#include <optional>
+
+#include "Constants.h"
+
+class BoardMove;
+class BoardSquare;
+struct PieceData;
+
+
+class BoardMoveFactory {
+public:
+    static std::unique_ptr<BoardMove> createStandardMove(BoardSquare const &fromSquare, BoardSquare const &toSquare, BoardSquare const &captureSquare, bool doesEnableEnpassant, PieceData const &movedPieceData, std::optional<PieceData> const &capturedPieceData = std::nullopt);
+    static std::unique_ptr<BoardMove> createPromotionMove(BoardSquare const &fromSquare, BoardSquare const &toSquare, BoardSquare const &captureSquare, PieceType promotionPieceType, bool doesEnableEnpassant, PieceData const &movedPieceData, std::optional<PieceData> const &capturedPieceData = std::nullopt);
+    static std::unique_ptr<BoardMove> createCastleMove(BoardSquare const &fromSquare, BoardSquare const &toSquare, BoardSquare const &captureSquare, BoardSquare const &rookFromSquare, BoardSquare const &rookToSquare,  bool doesEnableEnpassant, PieceData const &movedPieceData, std::optional<PieceData> const &capturedPieceData = std::nullopt);
+};
+
+
+#endif /* BoardMoveFactory_h */
