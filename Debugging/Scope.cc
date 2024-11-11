9aaf091cbfa28ffd1defcd242b91fe81a5a74a57
grechsteiner <grayson@fullscale.org>
2024-11-11 01:45:32 -0500

    Scope

---
+++ Debugging/Scope.cc
@@ -0,0 +1,3 @@
+// Scope.cc
+
+#include "Scope.h"
--- Debugging/Scope.h
+++ Debugging/Scope.h
@@ -9,7 +9,6 @@ class Scope {
     std::string s;
 
 public:
-
     Scope(std::string str) : s(str) { std::cout << "Starting: " << s << std::endl; }
     ~Scope() { std::cout << "Ending: " << s << std::endl; }
 
