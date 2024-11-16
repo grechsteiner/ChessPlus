95d18520ac5d93c060dd3f87bbbad85008c89538
grechsteiner <grayson@fullscale.org>
2024-11-16 16:48:56 -0500

    Refine cloning dir

--- Cloning/Cloneable.h
+++ Cloning/Cloneable.h
@@ -5,19 +5,27 @@
 
 #include <memory>
 
+
 /**
- * Used with direct inheritence
+ * Cloneable Class
+ * - CRTP Cloning
+ * - Abstract --> ComplicatedCloneable --> Concrete
  */
 template <typename Base, typename Derived>
 class Cloneable : public Base {
 private:
-    std::unique_ptr<Base> cloneImpl() const override {
-        return std::make_unique<Derived>(static_cast<Derived const&>(*this));
-    }
+    std::unique_ptr<Base> cloneImpl() const override;
+
 public:
     using Base::Base;
     virtual ~Cloneable() = 0;
 };
+
+
+template <typename Base, typename Derived>
+std::unique_ptr<Base> Cloneable<Base, Derived>::cloneImpl() const {
+    return std::make_unique<Derived>(static_cast<Derived const&>(*this));
+}
 
 template <typename Base, typename Derived>
 Cloneable<Base, Derived>::~Cloneable() {}
--- Cloning/ComplicatedCloneable.h
+++ Cloning/ComplicatedCloneable.h
@@ -5,21 +5,27 @@
 
 #include <memory>
 
+
 /**
- * Used when clone type is not base type
- * Unique ptrs can't implicitly upcast via covariance
- * Ex: Abstract --> Abstract --> Concrete
+ * Complicated Cloneable Class
+ * - CRTP Cloning
+ * - Abstract --> Abstract --> ComplicatedCloneable --> Concrete
  */
 template <typename Clone, typename Base, typename Derived>
 class ComplicatedCloneable : public Base {
 private:
-    std::unique_ptr<Clone> cloneImpl() const override {
-        return std::make_unique<Derived>(static_cast<Derived const&>(*this));
-    }
+    std::unique_ptr<Clone> cloneImpl() const override;
+
 public:
     using Base::Base;
     virtual ~ComplicatedCloneable() = 0;
 };
+
+
+template <typename Clone, typename Base, typename Derived>
+std::unique_ptr<Clone> ComplicatedCloneable<Clone, Base, Derived>::cloneImpl() const {
+        return std::make_unique<Derived>(static_cast<Derived const&>(*this));
+    }
 
 template <typename Clone, typename Base, typename Derived>
 ComplicatedCloneable<Clone, Base, Derived>::~ComplicatedCloneable() {}
