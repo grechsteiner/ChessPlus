df2ecd145e07c8ddca0ea11a2b4639bea3ed3944
grechsteiner <grayson@fullscale.org>
2024-11-16 17:03:00 -0500

    Clean BoardSquare

--- ChessBoard/BoardSquare.cc
+++ ChessBoard/BoardSquare.cc
@@ -1,23 +1,57 @@
 // BoardSquare.cc
 
+#include "BoardSquare.h"
+
+#include <cctype>
 #include <optional>
 #include <regex>
-#include <cctype>
-
-#include "BoardSquare.h"
+#include <string>
 
 
 // Basic ctor
-BoardSquare::BoardSquare(int boardRow, int boardCol) : boardRow(boardRow), boardCol(boardCol) {}
+BoardSquare::BoardSquare(int boardRow, int boardCol) : 
+    boardRow(boardRow), boardCol(boardCol) { }
 
-// Equality operator
+// Copy ctor
+BoardSquare::BoardSquare(BoardSquare const &other) : 
+    boardRow(other.boardRow), boardCol(other.boardCol) { }
+
+// Move ctor
+BoardSquare::BoardSquare(BoardSquare &&other) noexcept :
+    boardRow(other.boardRow), boardCol(other.boardCol) { }
+
+// Copy assignment
+BoardSquare& BoardSquare::operator=(BoardSquare const &other) {
+    if (this != &other) {
+        boardRow = other.boardRow;
+        boardCol = other.boardCol;
+    }
+    return *this;
+}
+
+// Move assignment
+BoardSquare& BoardSquare::operator=(BoardSquare &&other) noexcept {
+    if (this != &other) {
+        boardRow = other.boardRow;
+        boardCol = other.boardCol;
+    }
+    return *this;
+}
+
+// Equality
 bool BoardSquare::operator==(BoardSquare const &other) const {
     return 
         boardRow == other.boardRow &&
         boardCol == other.boardCol;
 }
 
-// Static
+/*
+ * Static
+ *
+ * Create Optional BoardSquare
+ * - value if valid string representation
+ * - nullopt if in-valid string representation
+ */
 std::optional<BoardSquare> BoardSquare::createBoardSquare(std::string const &squareStr, int numRowsOnBoard, int numColsOnBoard) {
     static std::regex const boardSquarePattern("^([a-z]+)([1-9][0-9]*)$", std::regex_constants::icase);
     
--- ChessBoard/BoardSquare.h
+++ ChessBoard/BoardSquare.h
@@ -4,21 +4,23 @@
 #define BoardSquare_h
 
 #include <optional>
+#include <string>
 
 
 /**
  * BoardSquare Class
- * Represents a square in a 2D vector representing a chessboard
+ * - Represents a square on a ChessBoard (2D Vector)
+ * - Top left = (0, 0)
  */
 struct BoardSquare final {
     int boardRow;
     int boardCol;
 
     explicit BoardSquare(int boardRow, int boardCol);
-    BoardSquare(BoardSquare const &other) = default;
-    BoardSquare(BoardSquare &&other) noexcept = default;
-    BoardSquare& operator=(BoardSquare const &other) = default;
-    BoardSquare& operator=(BoardSquare &&other) noexcept = default;
+    BoardSquare(BoardSquare const &other);
+    BoardSquare(BoardSquare &&other) noexcept;
+    BoardSquare& operator=(BoardSquare const &other);
+    BoardSquare& operator=(BoardSquare &&other) noexcept;
     ~BoardSquare() = default;
 
     bool operator==(BoardSquare const &other) const;
