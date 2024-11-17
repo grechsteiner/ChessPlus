96a917d9eb88d651a7f87f2708f890b7c84512c2
grechsteiner <grayson@fullscale.org>
2024-11-17 16:40:36 -0500

    MoveDirection struct

---
+++ Pieces/MoveDirection.cc
@@ -0,0 +1,46 @@
+// MoveDirection.cc
+
+#include "MoveDirection.h"
+
+
+// Basic ctor
+MoveDirection::MoveDirection(int rowDirection, int colDirection) :
+    rowDirection(rowDirection), colDirection(colDirection) { }
+
+// Copy ctor
+MoveDirection::MoveDirection(MoveDirection const &other) :
+    rowDirection(other.rowDirection), colDirection(other.colDirection) { }
+
+// Move ctor
+MoveDirection::MoveDirection(MoveDirection &&other) noexcept :
+    rowDirection(other.rowDirection), colDirection(other.colDirection) { }
+
+// Copy assignmnet
+MoveDirection& MoveDirection::operator=(MoveDirection const &other) {
+    if (this != &other) {
+        rowDirection = other.rowDirection;
+        colDirection = other.colDirection;
+    }
+    return *this;
+}
+
+// Move assignmnet
+MoveDirection& MoveDirection::operator=(MoveDirection &&other) noexcept {
+    if (this != &other) {
+        rowDirection = other.rowDirection;
+        colDirection = other.colDirection;
+    }
+    return *this;
+}
+
+// Equality
+bool MoveDirection::operator==(MoveDirection const &other) const {
+    return
+        rowDirection == other.rowDirection &&
+        colDirection == other.colDirection;
+}
+
+// Inequality
+bool MoveDirection::operator!=(MoveDirection const &other) const {
+    return !(*this == other);
+}
---
+++ Pieces/MoveDirection.h
@@ -0,0 +1,26 @@
+// MoveDirection.h
+
+#ifndef MoveDirection_h
+#define MoveDirection_h
+
+/**
+ * MoveDirection Struct
+ * Represents a direction a Piece can move as part of a BoardMove
+ */
+struct MoveDirection final {
+    int rowDirection;
+    int colDirection;
+
+    explicit MoveDirection(int rowDirection, int colDirection);
+    MoveDirection(MoveDirection const &other);
+    MoveDirection(MoveDirection &&other) noexcept;
+    MoveDirection& operator=(MoveDirection const &other);
+    MoveDirection& operator=(MoveDirection &&other) noexcept;
+    ~MoveDirection() = default;
+
+    bool operator==(MoveDirection const &other) const;
+    bool operator!=(MoveDirection const &other) const;
+};
+
+
+#endif /* MoveDirection_h */
