0fb6893c864856db786df0d46ccc89870f3479c8
grechsteiner <grayson@fullscale.org>
2024-11-16 21:03:40 -0500

    Clean Utils

--- Utilities.cc
+++ Utilities.cc
@@ -1,14 +1,21 @@
 // Utilities.cc
 
-#include <string>
-#include <unordered_map>
+#include "Utilities.h"
+
 #include <algorithm>
 #include <cctype>
+#include <optional>
+#include <string>
+#include <unordered_map>
 
-#include "Utilities.h"
 #include "Constants.h"
 
 
+/*
+ * Return Optional ComputerPlayerLevel
+ * - value if string is associated with a ComputerPlayerLevel
+ * - nullopt otherwise
+ */
 std::optional<ComputerPlayerLevel> Utilities::stringToComputerPlayerLevel(std::string const &str) {
     static std::unordered_map<std::string, ComputerPlayerLevel> mapping = {
         { "1", ComputerPlayerLevel::ONE },
@@ -23,6 +30,11 @@ std::optional<ComputerPlayerLevel> Utilities::stringToComputerPlayerLevel(std::s
         : std::nullopt;
 }
 
+/*
+ * Return Optional PlayerType
+ * - value if string is associated with a PlayerType
+ * - nullopt otherwise
+ */
 std::optional<PlayerType> Utilities::stringToPlayerType(std::string const &str) {
     static std::unordered_map<std::string, PlayerType> mapping = {
         { "HUMAN", PlayerType::HUMAN },
@@ -36,6 +48,11 @@ std::optional<PlayerType> Utilities::stringToPlayerType(std::string const &str) 
         : std::nullopt;
 }
 
+/*
+ * Return Optional PieceType
+ * - value if string is associated with a PieceType
+ * - nullopt otherwise
+ */
 std::optional<PieceType> Utilities::stringToPieceType(std::string const &str) {
     static std::unordered_map<std::string, PieceType> mapping = {
         { "P", PieceType::PAWN },
@@ -53,6 +70,11 @@ std::optional<PieceType> Utilities::stringToPieceType(std::string const &str) {
         : std::nullopt;
 }
 
+/*
+ * Return Optional PieceLevel
+ * - value if string is associated with a PieceLevel
+ * - nullopt otherwise
+ */
 std::optional<PieceLevel> Utilities::stringToPieceLevel(std::string const &str) {
     static std::unordered_map<std::string, PieceLevel> mapping = {
         { "BASIC", PieceLevel::BASIC },
@@ -66,6 +88,11 @@ std::optional<PieceLevel> Utilities::stringToPieceLevel(std::string const &str) 
         : std::nullopt;
 }
 
+/*
+ * Return Optional PieceDirection
+ * - value if string is associated with a PieceDirection
+ * - nullopt otherwise
+ */
 std::optional<PieceDirection> Utilities::stringToPieceDirection(std::string const &str) {
     static std::unordered_map<std::string, PieceDirection> mapping = {
         { "NORTH", PieceDirection::NORTH },
--- Utilities.h
+++ Utilities.h
@@ -4,15 +4,17 @@
 #define Utilities_h
 
 #include <optional>
+#include <string>
 
 #include "Constants.h"
 
 
+/**
+ * Global Utility Methods
+ */
 namespace Utilities {
     std::optional<ComputerPlayerLevel> stringToComputerPlayerLevel(std::string const &str);
-
     std::optional<PlayerType> stringToPlayerType(std::string const &str);
-
     std::optional<PieceType> stringToPieceType(std::string const &str);
     std::optional<PieceLevel> stringToPieceLevel(std::string const &str);
     std::optional<PieceDirection> stringToPieceDirection(std::string const &str);
