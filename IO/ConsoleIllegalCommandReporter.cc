dd97939a11e7fb6c18be562f456a8acc6391cc11
grechsteiner <grayson@fullscale.org>
2024-11-16 22:18:48 -0500

    Clean console reporter

--- IO/ConsoleIllegalCommandReporter.cc
+++ IO/ConsoleIllegalCommandReporter.cc
@@ -1,23 +1,30 @@
 // ConsoleIllegalCommandReporter.cc
 
+#include "ConsoleIllegalCommandReporter.h"
+
 #include <iostream>
 #include <string>
 
-#include "ConsoleIllegalCommandReporter.h"
+#include "Cloneable.h"
+#include "IllegalCommandReporter.h"
 
 
 // Basic ctor
 ConsoleIllegalCommandReporter::ConsoleIllegalCommandReporter(std::ostream &out) : 
-    out(out) {}
+    Cloneable<IllegalCommandReporter,ConsoleIllegalCommandReporter>(), out(out) {}
 
 // Copy ctor
 ConsoleIllegalCommandReporter::ConsoleIllegalCommandReporter(ConsoleIllegalCommandReporter const &other) : 
-    Cloneable<IllegalCommandReporter,ConsoleIllegalCommandReporter>(), out(other.out) {}
+    Cloneable<IllegalCommandReporter,ConsoleIllegalCommandReporter>(other), out(other.out) {}
 
 // Move ctor
 ConsoleIllegalCommandReporter::ConsoleIllegalCommandReporter(ConsoleIllegalCommandReporter &&other) noexcept : 
-    Cloneable<IllegalCommandReporter,ConsoleIllegalCommandReporter>(), out(other.out) {}
+    Cloneable<IllegalCommandReporter,ConsoleIllegalCommandReporter>(std::move(other)), out(other.out) {}
 
+
+/*
+ * Output the illegal command report
+ */
 void ConsoleIllegalCommandReporter::reportIllegalCommandImpl(std::string const &message) {
     out << "Illegal Command: " << message << std::endl;
 }
--- IO/ConsoleIllegalCommandReporter.h
+++ IO/ConsoleIllegalCommandReporter.h
@@ -6,23 +6,25 @@
 #include <iostream>
 #include <string>
 
-#include "IllegalCommandReporter.h"
 #include "Cloneable.h"
+#include "IllegalCommandReporter.h"
 
 
 /**
  * ConsoleIllegalCommandReporter InvalidCommandReporter  Class
  */
-class ConsoleIllegalCommandReporter final : public Cloneable<IllegalCommandReporter,ConsoleIllegalCommandReporter> {
+class ConsoleIllegalCommandReporter final : public Cloneable<IllegalCommandReporter, ConsoleIllegalCommandReporter> {
 private:
     std::ostream &out;
+
     void reportIllegalCommandImpl(std::string const &message) override;
+
 public:
     explicit ConsoleIllegalCommandReporter(std::ostream &out);
     ConsoleIllegalCommandReporter(ConsoleIllegalCommandReporter const &other);
     ConsoleIllegalCommandReporter(ConsoleIllegalCommandReporter &&other) noexcept;
-    // Copy assignment not enabled (can't copy streams)
-    // Move assignment not enabled (can't move streams)
+    IllegalCommandReporter& operator=(IllegalCommandReporter const &other) = delete;
+    IllegalCommandReporter& operator=(IllegalCommandReporter &&other) = delete;
     virtual ~ConsoleIllegalCommandReporter() = default;
 };
 
