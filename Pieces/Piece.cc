8a0b91674504b20f81ccca832865cc269b2fbb76
grechsteiner <grayson@fullscale.org>
2024-11-17 16:05:14 -0500

    clean piece

--- Pieces/Piece.cc
+++ Pieces/Piece.cc
@@ -1,10 +1,12 @@
 // Piece.cc
 
-#include <vector>
+#include "Piece.h"
+
 #include <memory>
 #include <utility>
+#include <vector>
 
-#include "Piece.h"
+#include "PieceData.h"
 #include "PieceInfo.h"
 #include "ChessBoard.h"
 #include "BoardSquare.h"
@@ -13,11 +15,24 @@
 
 // Basic ctor
 Piece::Piece(PieceData const &pieceData, PieceInfo const &pieceInfo) :
-    pieceData(pieceData), pieceInfo(pieceInfo) {}
+    pieceData(pieceData), pieceInfo(pieceInfo) { }
+
+// Copy ctor
+Piece::Piece(Piece const &other) :
+    pieceData(other.pieceData), pieceInfo(other.pieceInfo) { }
 
 // Move ctor
 Piece::Piece(Piece &&other) noexcept :
-    pieceData(std::move(other.pieceData)), pieceInfo(std::move(other.pieceInfo)) {}
+    pieceData(std::move(other.pieceData)), pieceInfo(std::move(other.pieceInfo)) { }
+
+// Copy assignment
+Piece& Piece::operator=(Piece const &other) {
+    if (this != &other) {
+        pieceData = other.pieceData;
+        pieceInfo = other.pieceInfo;
+    }
+    return *this;
+}
 
 // Move assignment
 Piece& Piece::operator=(Piece &&other) noexcept {
@@ -28,8 +43,22 @@ Piece& Piece::operator=(Piece &&other) noexcept {
     return *this;
 }
 
+// Equality
+bool Piece::operator==(Piece const &other) const {
+    return
+        pieceData == other.pieceData &&
+        pieceInfo == other.pieceInfo;
+}
+
+// Inequality
+bool Piece::operator!=(Piece const &other) const {
+    return !(*this == other);
+}
+
+/* Public Virtual Methods */
 std::vector<std::unique_ptr<BoardMove>> Piece::getMoves(std::unique_ptr<ChessBoard> const &chessBoard, BoardSquare const &fromSquare, bool onlyAttackingMoves) const { return getMovesImpl(chessBoard, fromSquare, onlyAttackingMoves); }
 std::unique_ptr<Piece> Piece::clone() const { return cloneImpl(); }
 
+/* Getters */
 PieceData const& Piece::getPieceData() const { return pieceData; }
 PieceInfo const& Piece::getPieceInfo() const { return pieceInfo; }
--- Pieces/Piece.h
+++ Pieces/Piece.h
@@ -3,8 +3,8 @@
 #ifndef Piece_h
 #define Piece_h
 
-#include <vector>
 #include <memory>
+#include <vector>
 
 #include "PieceData.h"
 #include "PieceInfo.h"
@@ -12,13 +12,15 @@
 #include "BoardSquare.h"
 #include "BoardMove.h"
 
+
 /**
  * Abstract Piece Class
  * Represents a Piece on a ChessBoard
  */
 class Piece {
-
 private:
+    virtual std::vector<std::unique_ptr<BoardMove>> getStandardMoves(std::unique_ptr<ChessBoard> const &chessBoard, BoardSquare const &fromSquare, bool onlyAttackingMoves) const = 0;
+
     virtual std::vector<std::unique_ptr<BoardMove>> getMovesImpl(std::unique_ptr<ChessBoard> const &chessBoard, BoardSquare const &fromSquare, bool onlyAttackingMoves) const = 0;
     virtual std::unique_ptr<Piece> cloneImpl() const = 0;
     
@@ -27,21 +29,22 @@ protected:
     PieceInfo pieceInfo;
 
     explicit Piece(PieceData const &pieceData, PieceInfo const &pieceInfo);
-    Piece(Piece const &other) = default;
+    Piece(Piece const &other);
     Piece(Piece &&other) noexcept;
-    Piece& operator=(Piece const &other) = default;
+    Piece& operator=(Piece const &other);
     Piece& operator=(Piece &&other) noexcept;
 
-    virtual std::vector<std::unique_ptr<BoardMove>> getStandardMoves(std::unique_ptr<ChessBoard> const &chessBoard, BoardSquare const &fromSquare, bool onlyAttackingMoves) const = 0;
-
 public:
+    virtual ~Piece() = default;
+
+    bool operator==(Piece const &other) const;
+    bool operator!=(Piece const &other) const;
+
     std::vector<std::unique_ptr<BoardMove>> getMoves(std::unique_ptr<ChessBoard> const &chessBoard, BoardSquare const &fromSquare, bool onlyAttackingMoves) const;
     std::unique_ptr<Piece> clone() const;
     
     PieceData const& getPieceData() const;
     PieceInfo const& getPieceInfo() const;
-
-    virtual ~Piece() = default;
 };
 
 
