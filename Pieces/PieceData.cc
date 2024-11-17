b365fa145898b601afdb6e3a4bab6e74500beb75
grechsteiner <grayson@fullscale.org>
2024-11-17 16:20:38 -0500

    Clean PieceData

--- Pieces/PieceData.cc
+++ Pieces/PieceData.cc
@@ -1,19 +1,33 @@
 // PieceData.cc
 
-#include <string>
-#include <utility>
-
 #include "PieceData.h"
+
 #include "Constants.h"
 
 
 // Basic ctor
 PieceData::PieceData(PieceType pieceType, PieceLevel pieceLevel, Team team, PieceDirection pieceDirection, bool hasMoved) :
-    pieceType(pieceType), pieceLevel(pieceLevel), team(team), pieceDirection(pieceDirection), hasMoved(hasMoved) {}
+    pieceType(pieceType), pieceLevel(pieceLevel), team(team), pieceDirection(pieceDirection), hasMoved(hasMoved) { }
+
+// Copy ctor
+PieceData::PieceData(PieceData const &other) :
+    pieceType(other.pieceType), pieceLevel(other.pieceLevel), team(other.team), pieceDirection(other.pieceDirection), hasMoved(other.hasMoved) { }
 
 // Move ctor
 PieceData::PieceData(PieceData &&other) noexcept : 
-    pieceType(other.pieceType), pieceLevel(other.pieceLevel), team(other.team), pieceDirection(other.pieceDirection), hasMoved(other.hasMoved) {}
+    pieceType(other.pieceType), pieceLevel(other.pieceLevel), team(other.team), pieceDirection(other.pieceDirection), hasMoved(other.hasMoved) { }
+
+// Copy assignment
+PieceData& PieceData::operator=(const PieceData& other) {
+    if (this != &other) {
+        pieceType = other.pieceType;
+        pieceLevel = other.pieceLevel;
+        team = other.team;
+        pieceDirection = other.pieceDirection;
+        hasMoved = other.hasMoved;
+    }
+    return *this;
+}
 
 // Move assignment
 PieceData& PieceData::operator=(PieceData&& other) noexcept {
@@ -27,7 +41,7 @@ PieceData& PieceData::operator=(PieceData&& other) noexcept {
     return *this;
 }
 
-// Equality operator
+// Equality
 bool PieceData::operator==(PieceData const &other) const {
     return
         pieceType == other.pieceType &&
@@ -35,4 +49,9 @@ bool PieceData::operator==(PieceData const &other) const {
         team == other.team &&
         pieceDirection == other.pieceDirection &&
         hasMoved == other.hasMoved;
+}
+
+// Inequality
+bool PieceData::operator!=(PieceData const &other) const {
+    return !(*this == other);
 }
--- Pieces/PieceData.h
+++ Pieces/PieceData.h
@@ -3,14 +3,12 @@
 #ifndef PieceData_h
 #define PieceData_h
 
-#include <string>
-
 #include "Constants.h"
 
 
 /**
  * PieceData Struct
- * Represents all the data pertaining to a piece that is modifiable at construction time
+ * Represents all the data pertaining to a Piece that is modifiable at construction time
  */
 struct PieceData final {
     PieceType pieceType;
@@ -20,13 +18,14 @@ struct PieceData final {
     bool hasMoved;
 
     explicit PieceData(PieceType pieceType, PieceLevel pieceLevel, Team team, PieceDirection pieceDirection, bool hasMoved);
-    PieceData(PieceData const &other) = default;
+    PieceData(PieceData const &other);
     PieceData(PieceData &&other) noexcept;
-    PieceData& operator=(const PieceData& other) = default;
+    PieceData& operator=(const PieceData& other);
     PieceData& operator=(PieceData&& other) noexcept;
     ~PieceData() = default;
 
     bool operator==(PieceData const &other) const;
+    bool operator!=(PieceData const &other) const;
 };
 
 
